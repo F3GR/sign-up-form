4ea4edbd2191f50209915fc0f0f25d03ba3fa635
F3GR <egmver.2@gmail.com>
2023-07-03 23:12:56 +0400

    finished layout

---
+++ script.js
@@ -0,0 +1,63 @@
+const selectedForm = document.querySelector('.registration');
+const selectedButton = document.querySelector('.options-box button');
+const selectedFields = document.querySelectorAll('.field input');
+const selectedPassword = document.getElementById('password');
+const selectedPasswordConfirmation = document.getElementById('password-check');
+
+selectedFields.forEach((field) => {
+    field.addEventListener('focusout', function(e) {
+        e.preventDefault();
+        if (field.checkValidity()) {
+            field.classList.add('valid');
+            field.classList.remove('invalid');
+        } else {
+            field.classList.add('invalid');
+            field.classList.remove('valid');
+        }
+    });
+});
+
+selectedPasswordConfirmation.addEventListener('focusout', function(e) {
+    e.preventDefault();
+    checkEquality();
+});
+
+selectedPassword.addEventListener('focusout', function(e) {
+    e.preventDefault();
+    checkEquality();
+});
+
+selectedButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (selectedForm.checkValidity()) {
+        selectedForm.submit();
+  } else {
+    selectedForm.reportValidity();
+  }
+});
+
+selectedButton.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        if (selectedForm.checkValidity()) {
+            if (checkEquality()) {
+                selectedForm.submit();
+            }
+        } else {
+            selectedForm.reportValidity();
+        }
+    }
+});
+
+function checkEquality() {
+    if (selectedPassword === selectedPasswordConfirmation) {
+        selectedPasswordConfirmation.classList.add('valid');
+        selectedPasswordConfirmation.classList.remove('invalid');
+        return true;
+    } else {
+        selectedPasswordConfirmation.classList.add('invalid');
+        selectedPasswordConfirmation.classList.remove('valid');
+        selectedPasswordConfirmation.setAttribute('title', 'Confirmed password must be equal to the entered password');
+        return false;
+    }
+}
